7fd5c6890f807d0794379ef5f62565a07d7d7fd7
vincentius15 <vso_f1@yahoo.com>
2018-07-24 17:41:55 +0700

    Implement getOperation method

---
+++ operation.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/jmoiron/sqlx"
+)
+
+type operation struct {
+	ID          string `db:"id" json:"id"`
+	LxcID       string `db:"lxc_id" json:"lxc_id"`
+	Status      string `db:"status" json:"status"`
+	StatusCode  int    `db:"status_code" json:"status_code"`
+	Description string `db:"description" json:"description"`
+}
+
+func (op *operation) getOperation(db *sqlx.DB) error {
+	rows, err := db.Queryx("SELECT * FROM operation WHERE id=$1 LIMIT 1", op.ID)
+	if err != nil {
+		return err
+	}
+
+	if rows.Next() {
+		err = rows.StructScan(&op)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
---
+++ operation_test.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+var operationScheduler scheduler
+
+type OperationSuite struct {
+	suite.Suite
+}
+
+func TestOperationSuite(t *testing.T) {
+	suite.Run(t, new(OperationSuite))
+}
+
+func (suite *OperationSuite) SetupSuite() {
+	operationScheduler = scheduler{}
+	err := operationScheduler.initialize("postgres", "postgres", "saga", "localhost", "5432", "disable")
+	suite.NoError(err, "They should be no error")
+
+	clearQuery := `DELETE FROM operation;
+	DELETE FROM lxc;
+	DELETE FROM lxd;`
+
+	_, err = operationScheduler.DB.Exec(clearQuery)
+	suite.NoError(err, "They should be no error")
+
+	_, err = operationScheduler.DB.Exec("INSERT INTO lxd (id, name, address) VALUES ('very-unique-lxd-uuid', 'test-lxd', 'test.gojek.com');")
+	suite.NoError(err, "They should be no error")
+	_, err = operationScheduler.DB.Exec("INSERT INTO lxc (id, lxd_id, name, type, alias, is_deployed) VALUES ('very-unique-lxc-uuid','very-unique-lxd-uuid', 'test-lxc-1', 'image', '16.04', 0);")
+	suite.NoError(err, "They should be no error")
+}
+
+func (suite *OperationSuite) TearDownSuite() {
+	clearQuery := `DELETE FROM operation;
+	DELETE FROM lxc;
+	DELETE FROM lxd;`
+
+	_, err := lxcScheduler.DB.Exec(clearQuery)
+	suite.NoError(err, "They should be no error")
+}
+
+func (suite *OperationSuite) TestGetOperationSuccessful() {
+	testLxc := lxc{
+		ID: "1",
+	}
+
+	err := testLxc.getLxc(lxcScheduler.DB)
+	suite.NoError(err, "They should be no error")
+
+}
