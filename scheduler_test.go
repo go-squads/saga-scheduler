e556048bc45b8dd5c51397de2d13d8ca2c6161b3
vincentius15 <vso_f1@yahoo.com>
2018-07-25 09:52:17 +0700

    Create tests for initialize function (init db connection)

--- scheduler_test.go
+++ scheduler_test.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+var s scheduler
+
+type SchedulerSuite struct {
+	suite.Suite
+}
+
+func TestSchedulerSuite(t *testing.T) {
+	suite.Run(t, new(SchedulerSuite))
+}
+
+func (suite *SchedulerSuite) SetupSuite() {
+
+}
+
+func (suite *SchedulerSuite) TearDownSuite() {
+	s.DB.Close()
+}
+
+func (suite *SchedulerSuite) TestInitializeSuccessful() {
+	err := s.initialize("postgres", "postgres", "saga", "localhost", "5432", "disable")
+	suite.NoError(err, "They should be no error")
+}
+
+func (suite *SchedulerSuite) TestInitializeFailed() {
+	err := s.initialize("postgres", "postgres", "saga", "localhost", "80", "disable")
+	suite.Error(err, "They should be error")
+}
