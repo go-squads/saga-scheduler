5d04c13727bc8402de18a5e2db44c58c870a5453
Reynaldi Ananda Pane <reynaldipane@Reynaldis-MacBook-Pro.local>
2018-08-19 02:10:45 +0700

    [vin/rey] Refactor main to use os env variable

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
+import (
+	"os"
+)
+
 func main() {
 	saga := scheduler{}
-	saga.initialize("postgres", "postgres", "saga", "localhost", "5432", "disable")
+	saga.initialize(os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_URL"), os.Getenv("DB_PORT"), os.Getenv("DB_SSL_MODE"))
 	saga.run(":9300")
 }
